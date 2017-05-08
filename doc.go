8860f39c90acd54de57ac8d67e536539db34d452
Kevin Burke <kev@inburke.com>
2017-05-07 18:27:10 -0700

    update doc

--- doc.go
+++ doc.go
@@ -1,4 +1,9 @@
 // Package h256only implements a JWT-like parser with no configurability.
 //
+// The goal is to preserve the good part of JWT (a specification for sharing
+// data with a signature in a web-safe way) while avoiding the bad parts (pretty
+// much everything else). As a result, this library lacks many of the "features"
+// commonly found in JWT libraries.
+//
 // See README.md for more info.
 package h256only
